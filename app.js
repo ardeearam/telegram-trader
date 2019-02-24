d400f66eddd01fc7c426da07cada1a3af2cdb8e3
Ardee Aram <ardee.aram@gmail.com>
2019-02-24 11:47:22 +0000

    Added bitmex connectivity + implemented /quote feature

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const Test = require('./lib/test');
-const test = new Test();
-test.start();
+const Trader = require('./lib/trader');
+const network = process.env.TRADER_NETWORK || "testnet";
+const trader = new Trader({network: network});
+trader.start();
+
+module.exports = (() => Trader)();
